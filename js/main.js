8d7299aa5e4f430219667af416ae3c1769b91e4e
shaileshpujari <shaileshpuzari>
2022-01-22 19:32:14 +0545

    beta 1.1

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
-const hamburger = document.querySelector(".hamburger")
-const sideNav = document.querySelector('.side_nav')
-const main = document.querySelector('.main')
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    const hamburger = document.querySelector(".hamburger")
+    const sideNav = document.querySelector('.side_nav')
+    const main = document.querySelector('.main')
 
 
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('is-active')
-    sideNav.classList.toggle('side_nav_is_active')
-    main.classList.toggle('slide_left')
-})
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('is-active')
+        sideNav.classList.toggle('side_nav_is_active')
+        main.classList.toggle('slide_left')
+    })
 
 
-window.addEventListener('load', () => {
-    const preloadWrap = document.querySelector('.preload_wrap')
-
-    preloadWrap.classList.add('preload_finish')
-
+    window.addEventListener('load', () => {
+        const preloadWrap = document.querySelector('.preload_wrap')
+        preloadWrap.classList.add('preload_finish')
+        main.classList.add('preloadMain')
+    })
 })
