6ebdd028278efd26100a2081fe0daa5859a333a2
shaileshpujari <shaileshpuzari>
2022-01-23 20:06:12 +0545

    'beta'

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 
 document.addEventListener('DOMContentLoaded', () => {
+    const canvasWrapper = document.querySelector('.left_content .inner_content')
 
+    window.addEventListener('scroll', () => {
+        console.log('hello')
+        if (window.scrollY) {
+            canvasWrapper.classList.add('scrollCanvas')
+        }
+        else {
+            canvasWrapper.classList.remove('scrollCanvas')
+        }
+    })
 
     const hamburger = document.querySelector(".hamburger")
     const sideNav = document.querySelector('.side_nav')
     const main = document.querySelector('.main')
-
-
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('is-active')
@@ -15,22 +23,52 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
 
-    window.addEventListener('load', () => {
-        const preloadWrap = document.querySelector('.preload_wrap')
-        preloadWrap.classList.add('preload_finish')
-        main.style.opacity = 1;
-    })
     const canvas = document.querySelector('.picture_box .img_wrap')
     const myAnimation = new hoverEffect({
         parent: canvas,
         intensity: .5,
         image1: 'img/main_img.jpg',
         image2: 'img/main_img2.jpg',
-        displacementImage: 'img/displacement/10.jpg'
+        displacementImage: 'img/displacement/10.jpg',
     });
 
 
     AOS.init({
         duration: 1000,
     });
+
+    const accs = document.querySelectorAll(".accordion");
+
+    accs.forEach(
+        acc => {
+            acc.addEventListener("click", () => {
+                const panel = acc.nextElementSibling;
+                if (acc.classList.contains('active')) {
+                    panel.style.maxHeight = null;
+                    acc.classList.toggle("active");
+                }
+                else {
+                    accs.forEach(
+                        acc => {
+                            const panel = acc.nextElementSibling;
+                            acc.classList.remove('active')
+                            panel.style.maxHeight = null;
+                        })
+                    acc.classList.toggle("active");
+
+                    if (panel.style.maxHeight) {
+                        panel.style.maxHeight = null;
+                    } else {
+                        panel.style.maxHeight = panel.scrollHeight + "px";
+                    }
+                }
+            });
+        }
+    )
+
+    window.addEventListener('load', () => {
+        const preloadWrap = document.querySelector('.preload_wrap')
+        preloadWrap.classList.add('preload_finish')
+        main.style.opacity = 1;
+    })
 })
